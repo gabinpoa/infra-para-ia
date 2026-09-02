6a97cab842a62c71ee91594cc7478152a88871fd
Gabriel Nascimento <99542800+gabinpoa@users.noreply.github.com>
2026-09-02 20:10:38 +0000

    Atividade aula 1: Adicionar endpoint /sobre identificando a dupla

--- aula1-containers/api.py
+++ aula1-containers/api.py
@@ -47,3 +47,7 @@ def prediz(entrada: Entrada):
         sentimento=modelo.classes_[indice],
         confianca=round(float(probabilidades[indice]), 4),
     )
+
+@app.get("/sobre")
+def sobre():
+    return {"alunos": ["Isabela Canal", "Gabriel Nascimento"], "aula": 1}
